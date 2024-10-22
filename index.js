7fe617a22bf6ebad1a91080fec30d22ef1c481e2
Sinnema1 <jmanning@email.arizona.edu>
2024-10-22 10:01:04 -0700

    update formatting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import inquirer from 'inquirer';
-import fs from 'fs';
+import inquirer from "inquirer";
+import fs from "fs";
 
 // Function to generate the README content
 const generateReadme = (response, licenseBadge) => `
@@ -50,72 +50,76 @@ ${response.testSteps}
 
 // Prompt the user and write the README file
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your Project?',
-            name: 'title'
-        },
-        {
-            type: 'input',
-            message: 'Provide a description of your project.',
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: 'What are your project installation instructions?',
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: 'Please explain your project usage information.',
-            name: 'usage'
-        },
-        {
-            type: 'input',
-            message: 'What are the project contribution guidelines?',
-            name: 'contribution'
-        },
-        {
-            type: 'input',
-            message: 'Please explain the project test instructions.',
-            name: 'testSteps'
-        },
-        {
-            type: 'list',
-            message: 'Which license are you using?',
-            name: 'license',
-            choices: ["MIT", "ISC", "Mozilla", "IBM"]
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'github'
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email'
-        }
-    ])
-    .then((response) => {
-        // Determine the license badge
-        let licenseBadge;
-        if (response.license === "MIT") {
-            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        } else if (response.license === "ISC") {
-            licenseBadge = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-        } else if (response.license === "Mozilla") {
-            licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-        } else if (response.license === "IBM") {
-            licenseBadge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
-        }
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your Project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Provide a description of your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are your project installation instructions?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Please explain your project usage information.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What are the project contribution guidelines?",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Please explain the project test instructions.",
+      name: "testSteps",
+    },
+    {
+      type: "list",
+      message: "Which license are you using?",
+      name: "license",
+      choices: ["MIT", "ISC", "Mozilla", "IBM"],
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ])
+  .then((response) => {
+    // Determine the license badge
+    let licenseBadge;
+    if (response.license === "MIT") {
+      licenseBadge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (response.license === "ISC") {
+      licenseBadge =
+        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    } else if (response.license === "Mozilla") {
+      licenseBadge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else if (response.license === "IBM") {
+      licenseBadge =
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+    }
 
-        // Generate the README content
-        const readmeContent = generateReadme(response, licenseBadge);
+    // Generate the README content
+    const readmeContent = generateReadme(response, licenseBadge);
 
-        // Write the README file
-        fs.writeFile('SAMPLE.md', readmeContent, (err) => 
-            err ? console.error(err) : console.log('README created!')
-        );
-    });
+    // Write the README file
+    fs.writeFile("SAMPLE.md", readmeContent, (err) =>
+      err ? console.error(err) : console.log("README created!")
+    );
+  });
